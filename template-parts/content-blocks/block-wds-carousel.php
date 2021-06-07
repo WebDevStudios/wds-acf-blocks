f27767a70d9ed4c0e983f64e6f4cf429def2094e
JC <jc@webdevstudios.com>
2021-06-08 00:36:16 +0800

    #28 - Cleanup Carousel block

--- template-parts/content-blocks/block-wds-carousel.php
+++ template-parts/content-blocks/block-wds-carousel.php
@@ -21,22 +21,11 @@ if ( have_rows( 'carousel_slides' ) ) :
 				the_row();
 
 				// Set up fields.
-				$color         = get_sub_field( 'font_color' );
-				$block_title   = get_sub_field( 'title' );
-				$text          = get_sub_field( 'text' );
-				$button        = get_sub_field( 'button_link' );
-				$other_options = get_sub_field( 'other_options' ) ? get_sub_field( 'other_options' ) : get_field( 'other_options' )['other_options'];
-				$classname     = $color && wds_acf_blocks_has_array_key( 'color_picker', $color ) ? 'has-font-color color-' . esc_attr( $color['color_picker'] ) : '';
-
-				// If the block has expired, then bail!
-				if ( wds_acf_blocks_has_block_expired(
-					array(
-						'start_date' => $other_options['start_date'],
-						'end_date'   => $other_options['end_date'],
-					)
-				) ) {
-					continue;
-				}
+				$color       = get_sub_field( 'font_color' );
+				$block_title = get_sub_field( 'title' );
+				$text        = get_sub_field( 'text' );
+				$button      = get_sub_field( 'button_link' );
+				$classname   = $color && wds_acf_blocks_has_array_key( 'color_picker', $color ) ? 'has-font-color color-' . esc_attr( $color['color_picker'] ) : '';
 
 				// Start a <container> with possible block options.
 				wds_acf_blocks_display_block_options(
