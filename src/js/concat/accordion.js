d1d7acd684c4ed4722bc797d95924377c30a97c7
JC <jc@webdevstudios.com>
2021-06-09 02:42:30 +0800

    #28 Make accordion accessible

--- src/js/concat/accordion.js
+++ src/js/concat/accordion.js
@@ -1,60 +1,78 @@
 /**
- * Accordion block functionality
+ * File accordion.js
  *
- * @since January 31, 2020
- * @author Shannon MacMillan, Corey Collins
+ * Deal with ACF Block accordions.
+ */
+
+// Make sure everything is loaded first.
+if (
+	( 'complete' === document.readyState ||
+		'loading' !== document.readyState ) &&
+	! document.documentElement.doScroll
+) {
+	wdsAccordion();
+} else {
+	document.addEventListener( 'DOMContentLoaded', wdsAccordion );
+}
+
+/**
+ * Accessible Accordion block functionality.
+ *
+ * @author JC Palmes
+ * @since May 25, 2021
  */
 function wdsAccordion() {
-	const accordionItems = document.querySelectorAll( '.accordion-item' ),
-		accordionItemContent = document.querySelectorAll( '.accordion-item-content' );
-
-	// Loop through each accordion on the page and add a listener for its header.
-	accordionItems.forEach( ( accordion ) => {
-		const accordionHeader = accordion.querySelector( '.accordion-item-header' );
-
-		accordionHeader.addEventListener( 'click', toggleAccordion );
-	} );
+	const accordionTrigger = document.querySelectorAll( '.accordion-item-toggle' ),
+		accordionBody = document.querySelectorAll( '.accordion-item-body' );
 
 	// Open the hash link if one exists.
 	openHashLink();
 
-	/**
-	 * Handle toggling the accordion.
-	 *
-	 * @param {Object} event The targeted element.
-	 *
-	 * @since January 31, 2020
-	 * @author Shannon MacMillan, Corey Collins
-	 */
-	function toggleAccordion( event ) {
-		accordionItemContent.forEach( function( content ) {
-			const targetParent = event.target.parentNode.closest( '.accordion-item-header' );
-
-			// If we're clicking on this accordion...
-			if ( content.previousElementSibling === targetParent ) {
-
-				// If it's already opened, close it. Otherwise, open it!
-				if ( 'false' === content.getAttribute( 'aria-hidden' ) ) {
-					content.setAttribute( 'aria-hidden', 'true' );
-					content.parentElement.classList.remove( 'open' );
+	const accordionsToggle = ( triggers, bodyElements ) => {
+		triggers.forEach( ( e ) => {
+			e.addEventListener( 'click', () => {
+				e.disabled = true;
+				setTimeout( () => {
+					e.disabled = false;
+				}, 500 );
+				const eBody = e
+					.closest( '.accordion-item' )
+					.querySelector( '.accordion-item-body' );
+				e.classList.toggle( 'accordion-item-toggle--active' );
+				eBody.style.height = `${ eBody.scrollHeight }px`;
+				if (
+					( '0px' === eBody.style.height ||
+						'0px' === window.getComputedStyle( eBody ).height ) &&
+					'true' === eBody.getAttribute( 'aria-hidden' )
+				) {
+					e.setAttribute( 'aria-expanded', 'true' );
+					eBody.setAttribute( 'aria-hidden', 'false' );
 				} else {
-					content.setAttribute( 'aria-hidden', 'false' );
-					content.parentElement.classList.add( 'open' );
+					eBody.style.height = '0';
+					e.setAttribute( 'aria-expanded', 'false' );
+					eBody.setAttribute( 'aria-hidden', 'true' );
 				}
-			} else {
-				content.setAttribute( 'aria-hidden', 'true' );
-				content.parentElement.classList.remove( 'open' );
-			}
+			} );
 		} );
-	}
+		bodyElements.forEach( ( e ) => {
+			e.addEventListener( 'transitionend', () => {
+				if ( '0px' !== e.style.height ) {
+					e.style.height = 'auto';
+				}
+			} );
+		} );
+	};
+
+	accordionsToggle( accordionTrigger, accordionBody );
 }
 
 /**
- * Checks for a hash link in the URL and if one exists and matches an accordion, opens that accordion item.
+ * Checks for a hash link in the URL.
+ * If one exists and matches an accordion,
+ * opens that accordion item.
  *
- * @since January 31, 2020
  * @author Shannon MacMillan, Corey Collins
- *
+ * @since January 31, 2020
  * @return {boolean} Early bail of no hash.
  */
 function openHashLink() {
@@ -64,15 +82,17 @@ function openHashLink() {
 
 	const hashAccordionItem = document.querySelector( window.location.hash ),
 		hashAccordionItemHeader = hashAccordionItem.previousElementSibling,
-		hashAccordionItemButton = hashAccordionItemHeader.querySelector( '.accordion-item-toggle' );
+		hashAccordionItemButton = hashAccordionItemHeader.querySelector(
+			'.accordion-item-toggle'
+		);
 
 	hashAccordionItemButton.click();
 }
 
 // Handles ACF + Goots backend integration.
 if ( window.acf ) {
-	window.acf.addAction( 'render_block_preview/type=wds-accordion', wdsAccordion );
+	window.acf.addAction(
+		'render_block_preview/type=wds-accordion',
+		wdsAccordion
+	);
 }
-
-// Fire off our function.
-wdsAccordion();
