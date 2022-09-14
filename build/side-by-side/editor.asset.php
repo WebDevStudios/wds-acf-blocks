7e43586cc9c5cf1df7088d9e11799104bd654be3
Jenna Hines <jenna.hines@webdevstudios.com>
2022-09-14 13:58:08 -0400

    Updates side by side

--- build/side-by-side/editor.asset.php
+++ build/side-by-side/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7b3f8d8be0a44a9aa13f');
+<?php return array('dependencies' => array(), 'version' => 'fba64711e6a9ed1a536c');
---
+++ build/side-by-side/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2064df6f38369d08272b');
