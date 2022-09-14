847fd69ed6957b30a8027199ff19fb8f90f9831c
Jenna Hines <jenna.hines@webdevstudios.com>
2022-09-14 13:50:35 -0400

    Adds minimal frontend styles to cards repeater

--- build/cards-repeater/editor.asset.php
+++ build/cards-repeater/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'e7ffbc7412aeb8b2a87c');
+<?php return array('dependencies' => array(), 'version' => '20efd865496b3d429af0');
---
+++ build/cards-repeater/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8c9a93d329a26e0441bf');
