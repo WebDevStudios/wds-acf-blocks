28686b9178d04361719712ec1baf65af1a13dcd0
Jenna Hines <jenna.hines@webdevstudios.com>
2022-09-19 15:43:08 -0400

    Adds postcss config

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	plugins: {
+		'postcss-multiple-tailwind': {
+			mode: 'auto',
+		},
+		autoprefixer: { grid: true },
+		...(process.env.NODE_ENV === 'production'
+			? {
+				cssnano: {
+					preset: [
+						'default',
+						{
+							discardComments: {
+								removeAll: true,
+							},
+						},
+					],
+				},
+			}
+			: {}),
+	},
+};
