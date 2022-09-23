1cdf81da6b5ab016436e0e9fec069010349afd87
Oliver Harrison <oliver@positivebias.com>
2022-09-23 15:26:05 -0400

    Enqueue editor styles for Admin pages, not frontend

--- inc/helpers/scripts.php
+++ inc/helpers/scripts.php
@@ -14,7 +14,7 @@ namespace WebDevStudios\abs;
  */
 function scripts_styles() {
 
-	// Register global plugin styles.
+	// Enqueue global plugin styles for the Admin.
 	wp_enqueue_style( 'abs-styles', ABS_ROOT_URL . '/assets/editor-styles.css', [], ABS_WDS_ACF_VERSION );
 }
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\scripts_styles' );
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\scripts_styles' );
