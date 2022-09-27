33b6f2c4d8fd063bbf61d160c5e2313876c7fe07
Oliver Harrison <oliver@positivebias.com>
2022-09-27 12:51:01 -0400

    Fixes spacing issues to eliminate linting errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-const path         = require( 'path' );
-global.themePreset = path.join( __dirname, './../../themes/wd_s/wds.preset.js' );
-global.appRoot     = path.resolve( __dirname );
+const path = require( 'path' );
+global.themePreset = path.join(
+	__dirname,
+	'./../../themes/wd_s/wds.preset.js'
+);
+global.appRoot = path.resolve( __dirname );
 
 module.exports = {
 	plugins: {
@@ -8,19 +11,19 @@ module.exports = {
 			mode: 'auto',
 		},
 		autoprefixer: { grid: true },
-		...(process.env.NODE_ENV === 'production'
+		...( process.env.NODE_ENV === 'production'
 			? {
-				cssnano: {
-					preset: [
-						'default',
-						{
-							discardComments: {
-								removeAll: true,
+					cssnano: {
+						preset: [
+							'default',
+							{
+								discardComments: {
+									removeAll: true,
+								},
 							},
-						},
-					],
-				},
-			}
-			: {}),
+						],
+					},
+			  }
+			: {} ),
 	},
 };
