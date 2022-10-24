c2b4b8c8363477dabb0a19e33dd01d876639c7a9
Oliver Harrison <oliver@positivebias.com>
2022-10-24 14:50:55 -0400

    Rename $post_id -> $block_id

--- inc/helpers/get-acf-fields.php
+++ inc/helpers/get-acf-fields.php
@@ -11,20 +11,20 @@ namespace WebDevStudios\abs;
  * Returns an array of ACF fields.
  *
  * @param array $fields Array of field names ie: [ 'layout', 'eyebrow_heading', 'content', 'heading' ].
- * @param int   $post_id (optional) ID of the post or of the block ($block[id]).
+ * @param int   $block_id (optional) ID of the post or of the block ($block[id]).
  *
  * @author Adam Bates <adam.bates@webdevstudios.com>
  */
-function get_acf_fields( $fields = [], $post_id = false ) {
+function get_acf_fields( $fields = [], $block_id = false ) {
 
 	if ( ! function_exists( 'get_field' ) ) :
 		return;
 	endif;
 
-	$post_id       = $post_id ? $post_id : get_the_ID();
+	$block_id      = $block_id ? $block_id : get_the_ID();
 	$return_fields = [];
 	foreach ( $fields as $field ) :
-		$value                   = get_field( $field, $post_id );
+		$value                   = get_field( $field, $block_id );
 		$return_fields[ $field ] = $value;
 	endforeach;
 
