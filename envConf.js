cf0407235084a7512bb895cdce7dafa83ed3c6f0
bplv112 <bplv.me@gmail.com>
2022-10-26 13:20:34 -0500

    Fixed :: Es lint errors

--- envConf.js
+++ envConf.js
@@ -1,34 +1,30 @@
 // Require dotenv.
-require('dotenv').config();
+require( 'dotenv' ).config();
 
-const fs         = require( 'fs' );
-const path       = require( 'path' );
-const request    = require('request');
-const themeUrl   = 'https://raw.githubusercontent.com/WebDevStudios/wd_s/main/wds.preset.js';
-var downloadPath = 'fallbackPreset.js';
+const fs = require( 'fs' );
+const path = require( 'path' );
+const request = require( 'request' );
+const themeUrl =
+	'https://raw.githubusercontent.com/WebDevStudios/wd_s/main/wds.preset.js';
+const downloadPath = 'fallbackPreset.js';
 
-if ( ! fs.existsSync( 'fallbackPreset.js' ) ){
-	request(themeUrl)
-		.pipe(fs.createWriteStream(downloadPath))
-		.on('close', function () {
-			console.log('Fallback preset created.');
-	});
+if ( ! fs.existsSync( 'fallbackPreset.js' ) ) {
+	request( themeUrl )
+		.pipe( fs.createWriteStream( downloadPath ) )
+		.on( 'close', function () {} );
 }
 
 // set presetContents
-let envVars   = 'activePrest="' + path.join(
-	__dirname,
-	'./../../themes/wd_s/wds.preset.js"' + '\r\n'
-);
+let envVars =
+	'activePrest="' +
+	path.join( __dirname, './../../themes/wd_s/wds.preset.js"' + '\r\n' );
 
-envVars += 'fallbackPreset="' + path.join(
-	__dirname,
-	'./fallbackPreset.js"' + '\r\n'
-);
+envVars +=
+	'fallbackPreset="' +
+	path.join( __dirname, './fallbackPreset.js"' + '\r\n' );
 
-fs.writeFileSync(".env", envVars, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-});
-
+fs.writeFileSync( '.env', envVars, function ( err ) {
+	if ( err ) {
+		return err;
+	}
+} );
