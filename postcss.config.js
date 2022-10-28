d33cfd1ebae0125d625cad1027e417ba1d122771
Jenna Hines <jenna.hines@webdevstudios.com>
2022-10-28 09:50:49 -0400

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,25 @@
 require( 'dotenv' ).config();
 
+//THIS DOES NOT WORK
+const fs = require( 'fs' );
 const globalThemePreset = fs.existsSync( process.env.activePreset )
-	? require( process.env.activePreset )
-	: require( process.env.fallbackPreset );
+	? process.env.activePreset
+	: process.env.fallbackPreset;
+
+
+//THIS WORKS
+// const globalThemePreset = '/Users/jennahines/Sites/wds-acf-blocks/app/public/wp-content/themes/wd_s/wds.preset.js';
+
+
+//THIS WORKS
+// const path = require( 'path' );
+// global.appRoot = path.resolve( __dirname );
+// const globalThemePreset = path.join(
+// 	global.appRoot,
+// 	'./../../themes/wd_s/wds.preset.js'
+// );
+
+console.log(globalThemePreset);
 
 module.exports = {
 	plugins: {
