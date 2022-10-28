c0a7a49ce02b6f15f0641665a919b3759bf59521
Oliver Harrison <oliver@positivebias.com>
2022-10-28 09:12:46 -0400

    Add env configuration back; require all the things

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
+require( 'dotenv' ).config();
+
 const path = require( 'path' );
-global.appRoot = path.resolve( __dirname );
-const globalThemePreset = path.join(
-	global.appRoot,
-	'./../../themes/wd_s/wds.preset.js'
-);
+// global.appRoot = path.resolve( __dirname );
+// const globalThemePreset = path.join(
+// 	global.appRoot,
+// 	'./../../themes/wd_s/wds.preset.js'
+// );
+const globalThemePreset = fs.existsSync( process.env.activePreset )
+	? require( process.env.activePreset )
+	: require( process.env.fallbackPreset );
 
 module.exports = {
 	plugins: {
